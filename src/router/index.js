ca9d5b8c2d96d375819204c37feeea79843fe869
varaeff <varaev.sergey@gmail.com>
2024-05-15 17:31:08 +0400

    views & layouts added

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,60 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
+    meta: { layout: "main" },
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "empty" },
+    component: () => import("@/views/LoginView.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "empty" },
+    component: () => import("@/views/RegisterView.vue"),
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("@/views/CategoriesView.vue"),
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("@/views/PlanningView.vue"),
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("@/views/HistoryView.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("@/views/ProfileView.vue"),
+  },
+  {
+    path: "/record",
+    name: "record",
+    meta: { layout: "main" },
+    component: () => import("@/views/RecordView.vue"),
+  },
+  {
+    path: "/detailrecord",
+    name: "detailrecord",
+    meta: { layout: "main" },
+    component: () => import("@/views/DetailRecordView.vue"),
   },
 ];
 
