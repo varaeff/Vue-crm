3c3c70537003a8cdf99acb461a959a1ba61d7239
varaeff <varaev.sergey@gmail.com>
2024-05-17 15:30:52 +0400

    firebase added && login & register methods realized

--- src/main.js
+++ src/main.js
@@ -6,4 +6,31 @@ import store from "./store";
 import "materialize-css/dist/js/materialize.min";
 import messagePlugin from "@/utils/messagePlugin";
 
-createApp(App).use(store).use(router).use(messagePlugin).mount("#app");
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAK6IL2iW6hyt43ST5CtEFDpDjTgW950EM",
+  authDomain: "vue-crm-3abcc.firebaseapp.com",
+  projectId: "vue-crm-3abcc",
+  storageBucket: "vue-crm-3abcc.appspot.com",
+  messagingSenderId: "1060555092993",
+  appId: "1:1060555092993:web:09b47339f4347d0cee3c00",
+  measurementId: "G-KDR11WH324",
+};
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+let app;
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App)
+      .use(store)
+      .use(router)
+      .use(messagePlugin)
+      .mount("#app");
+  }
+});
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import { createStore } from "vuex";
+import auth from "./auth";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    error: null,
+  },
+  getters: {
+    error: (s) => s.error,
+  },
+  mutations: {
+    setError(state, error) {
+      state.error = error;
+    },
+    clearError(state) {
+      state.error = null;
+    },
+  },
+  modules: {
+    auth,
+  },
 });
