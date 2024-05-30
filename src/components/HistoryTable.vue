751bddcfda42af6b8e1f8b51b7668e258cb57daa
varaeff <varaev.sergey@gmail.com>
2024-05-30 18:38:38 +0400

    history page basicly coded

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,49 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Сумма</th>
+        <th>Дата</th>
+        <th>Категория</th>
+        <th>Тип</th>
+        <th>Открыть</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="(rec, index) of records" :key="rec.id">
+        <td>{{ index + 1 }}</td>
+        <td>{{ rec.amount }}₽</td>
+        <td>{{ rec.date.split("T")[0] }}</td>
+        <td>{{ rec.categoryName }}</td>
+        <td>
+          <span class="white-text badge" :class="[rec.typeClacc]">{{
+            rec.typeName
+          }}</span>
+        </td>
+        <td>
+          <button
+            v-tooltip="'Посмотреть запись'"
+            class="btn-small btn"
+            @click="$router.push(`/detail/${rec.id}`)"
+          >
+            <i class="material-icons">open_in_new</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "history-table",
+  props: {
+    records: {
+      requred: true,
+      type: Array,
+    },
+  },
+};
+</script>
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -8,36 +8,40 @@
       <canvas></canvas>
     </div>
 
-    <section>
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-            <th>Категория</th>
-            <th>Тип</th>
-            <th>Открыть</th>
-          </tr>
-        </thead>
+    <Loader v-if="loading" />
 
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>1212</td>
-            <td>12.12.32</td>
-            <td>name</td>
-            <td>
-              <span class="white-text badge red">Расход</span>
-            </td>
-            <td>
-              <button class="btn-small btn">
-                <i class="material-icons">open_in_new</i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <p v-else-if="!records.length" class="center">Записей не найдено</p>
+
+    <section v-else>
+      <historyTable :records="records" />
     </section>
   </div>
 </template>
+
+<script>
+import HistoryTable from "@/components/HistoryTable.vue";
+export default {
+  name: "history-view",
+  components: { HistoryTable },
+  data: () => ({
+    loading: true,
+    records: [],
+    categories: [],
+  }),
+  async mounted() {
+    this.categories = await this.$store.dispatch("fetchCathegiries");
+    const records = await this.$store.dispatch("fetchRecords");
+
+    this.records = records.map((rec) => {
+      return {
+        ...rec,
+        categoryName: this.categories.find((cat) => cat.id === rec.categoryId)
+          .name,
+        typeClacc: rec.type === "income" ? "green" : "red",
+        typeName: rec.type === "income" ? "Доход" : "Расход",
+      };
+    });
+    this.loading = false;
+  },
+};
+</script>
